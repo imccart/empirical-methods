a99a3cd0a656a498c0a34bbecae63be5c52f4ce1
Ian McCarthy <ianmccarthy.econ@gmail.com>
2021-08-19 15:03:17 -0400

    update file structure

--- docs/_render.R
+++ docs/_render.R
@@ -3,4 +3,5 @@ library(googledrive)
 setwd(here("docs"))
 drive_download("Bibliography/BibTeX_Library.bib", overwrite=TRUE, path='book.bib')
 bookdown::render_book("index.Rmd", 
-                      "bookdown::bs4_book")
+                      "bookdown::bs4_book",
+                      output_dir="../docs")
