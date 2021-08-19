fef0fc8275acb644fc8d8770a47b90dfb5b16ac5
Ian McCarthy <ianmccarthy.econ@gmail.com>
2021-08-19 14:51:20 -0400

    update to docs folder for github deploy

--- docs/_render.R
+++ docs/_render.R
@@ -1,6 +1,6 @@
 library(here)
 library(googledrive)
-setwd(here("book"))
+setwd(here("docs"))
 drive_download("Bibliography/BibTeX_Library.bib", overwrite=TRUE, path='book.bib')
 bookdown::render_book("index.Rmd", 
                       "bookdown::bs4_book")
