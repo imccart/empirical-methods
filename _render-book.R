70b0e09e14582dfaa037f86593301307d44cbd8c
Ian McCarthy <ianmccarthy.econ@gmail.com>
2021-08-19 15:22:57 -0400

    trying directory output

--- _render-book.R
+++ _render-book.R
@@ -4,4 +4,4 @@ setwd(here("docs"))
 drive_download("Bibliography/BibTeX_Library.bib", overwrite=TRUE, path='book.bib')
 bookdown::render_book("index.Rmd", 
                       "bookdown::bs4_book",
-                      output_dir="../docs")
+                      output_dir="docs")
